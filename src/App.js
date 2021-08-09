ee63873183303ad4c2d51ba92ad05bf76ee41bd9
julio.mejia.contractor <julio.mejia.contractor@bbva.com>
2021-08-08 20:34:43 -0500

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import '../src/app.scss';
+import '../src/querys.scss';
+import Header from '../src/components/Header';
+import TabFloat from '../src/components/TabFloat';
+import DataInitial from '../src/helpers/DataInitial';
+import { FavoriteContact } from './containers/FavoriteContainer';
+/* const Home = lazy(() => import('../src/components/Home')); */
+const Staff = lazy(() => import('../src/components/Staff'));
+const Compañeros = lazy(() => import('../src/components/Compañeros'));
 
-function App() {
+function App({ students, staff, compañeros }) {
+  const [ModalAdd, setModalAdd] = useState(false);
+  const [ModalFav, setModalFav] = useState(false);
+
+  const handleClick = (action, value) => {
+    if (action === 'Agregar') {
+      setModalAdd(true)
+    } else {
+      setModalFav(value)
+    }
+  }
+
+  const handleClose = (action) => {
+    if (action === 'close') {
+      setModalAdd(false)
+    } else {
+      setModalFav(false)
+    }
+  }
+
+  const handleSaveContact = (info) => {
+    fetch("http://localhost:3004/amigos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(info)
+    })
+      .then(res => res.json())
+      .then(result => {
+        console.log(result, 'result');
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <section className="container">
+          <div className="card-container">
+            <Switch>
+              <Route exact path="/">
+                <Suspense fallback={<div>Loading...</div>}>
+                  {/* <Home handleSaveContact={handleSaveContact} handleClose={handleClose} modal={ModalAdd} students={students} /> */}
+                  <FavoriteContact handleSaveContact={handleSaveContact} handleClose={handleClose} modal={ModalAdd} students={students} />
+                </Suspense>
+              </Route>
+              <Route exact path="/staff">
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Staff students={staff} />
+                </Suspense>
+              </Route>
+              <Route exact path="/compañeros">
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Compañeros students={compañeros} />
+                </Suspense>
+              </Route>
+            </Switch>
+          </div>
+        </section>
+        <TabFloat fav={ModalFav} handleClose={handleClose} handleClick={handleClick} />
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default DataInitial(App);
